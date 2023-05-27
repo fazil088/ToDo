c35b63b46f4cb233541f1f1da3a80cb26a6e4ed7
Muhammed Fazil <mhdf088@gmail.com>
2023-05-27 22:33:51 +0530

    draft box will be added in the todo app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   const [todos,setTodos] = useState([])
   const [todo,setTodo] = useState('')
-  const [buttonName,setBtnName] = useState('')
+  const [deleteItem,setDelete] = useState([])
   const date =new Date()
 
   function textChange(e){
@@ -16,6 +16,8 @@ function App() {
     if(e.target.classList[1]=== 'fa-plus' || e.key === 'Enter'){
       const newTodo = {
         task : todo,
+        status : false,
+        id : Date.now(),
         time : date.toLocaleTimeString([],{ hour: 'numeric', minute: 'numeric' }),
         dateof : date.toLocaleDateString(undefined,{month:'short',day:'2-digit'})
       }
@@ -24,12 +26,13 @@ function App() {
     }
   }
 
-
   function removeItem(pos){
-    todos.splice(pos,1)
+    let [sValues] = todos.splice(pos,1)
     setTodos([...todos])
+    setDelete((pre)=>[...pre,sValues])
   }
 
+  
   return (
     <div className="app">
       <div className="mainHeading">
@@ -45,28 +48,49 @@ function App() {
       </div>
       <div className="input">
         <input value={todo}  onChange={textChange} onKeyPress={addItems} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={addItems} onMouseEnter={(e)=>setBtnName(e.key)} className="fas fa-plus"></i>
+        <i onClick={addItems} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        {
+        
           <ul>
             {
               todos.map((value,k)=>{
               return(
                   <li key={k}>
                     <div className='listEdit'>
-                      <h5>{value.task}</h5>
+                      <h5 className={value.status ? 'crossed' : ''}>{value.task}</h5>
                       <p>{value.dateof}   {value.time}</p>
                     </div>  
                     <div className='editing'>
-                    <input type="checkbox" />
-                    <i className='fa fa-trash' onClick={()=>{ removeItem(k) }}></i>
+                    <input value={value.status} type="checkbox" onChange={(e)=>{
+                      setTodos(todos.filter((obj)=>{
+                        if(obj.id === value.id){
+                          obj.status = e.target.checked;
+                        }
+                        return obj
+                      }))
+                    }} />
+                    <i className='fa fa-trash' onClick={(e)=>{ removeItem(e) }}></i>
                     </div>
-                  </li>  
+                  </li>
               )
               })
             }
           </ul>
+        
+      </div>
+
+      <div className='draft'>
+        <h2>Draft Box</h2>
+        {
+          deleteItem.map((value,k)=>{
+          return(
+            <div className='draft-list' key={k}>
+              <h4>{value.task}</h4>
+              <p>{value.dateof}  {value.time}</p>
+            </div>
+          )
+          })
         }
       </div>
     </div>
